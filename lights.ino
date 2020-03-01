f185e2bf2e3a99f79f09062dd599d6942de22c92
simon <scjk@btinternet.com>
2020-03-01 21:23:13 +0000

    Got time from NTP server working

--- lights.ino
+++ lights.ino
@@ -12,7 +12,8 @@ int status = WL_IDLE_STATUS;
 
 unsigned int localPort = 2390;
 
-IPAddress timeServer(129, 6, 15, 28);
+// IPAddress timeServer(129, 6, 15, 28); //time.nist.gov
+IPAddress timeServer(143, 210, 16, 201); //0.uk.pool.ntp.org
 
 const int NTP_PACKET_SIZE = 48;
 
@@ -41,11 +42,11 @@ void setup() {
     delay(10000);
   }
 
-//  Serial.println("Connected to wifi");
-//  printWifiStatus();
+  Serial.println("Connected to wifi");
+  printWifiStatus();
 
-//  Serial.println("\nStarting connection to server...");
-//  Udp.begin(localUdpPort);
+  Serial.println("\nStarting connection to server...");
+  Udp.begin(localPort);
   
 //  rtc.begin();
 
@@ -55,7 +56,37 @@ void setup() {
 }
 
 void loop() {
-  while(true);
+  sendNTPpacket(timeServer);
+  delay(1000);
+  if (Udp.parsePacket()) {
+    Serial.println("packet received");
+    Udp.read(packetBuffer, NTP_PACKET_SIZE);
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    unsigned long secsSince1900 = highWord <<16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = ");
+    Serial.println(secsSince1900);
+    
+    const unsigned long seventyYears = 2208988800UL;
+    unsigned long epoch = secsSince1900 - seventyYears;
+    Serial.print("Unix time = ");
+    Serial.println(epoch);
+
+    Serial.print("The UTC time is ");
+    Serial.print((epoch % 86400L) / 3600);
+    Serial.print(':');
+    if (((epoch % 3600) / 60) < 10) {
+      Serial.print('0');
+    }
+    Serial.print((epoch % 3600) / 60);
+    Serial.print(':');
+    if ((epoch % 60) < 10) {
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60);
+  }
+
+  delay(10000);
 }
 //  timed on/off
 //  set hours plus randomness
@@ -74,18 +105,33 @@ void loop() {
 //    blink tea-for-two rhythm.
     
 //  at 3am get time from ntp and reset rtc
-  
-//void printWifiStatus() {
-//  Serial.print("SSID: ");
-// Serial.println(WiFi.SSID());
 
-//  IPAddress ip = WiFi.localIP();
-//  Serial.print("IP Address: ");
-//  Serial.println(ip);
+unsigned long sendNTPpacket(IPAddress& address) {
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  packetBuffer[0] = 0b11100011;
+  packetBuffer[1] = 0;
+  packetBuffer[2] = 6;
+  packetBuffer[3] = 0xEC;
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  Udp.beginPacket(address, 123);
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
 
-//  long rssi = WiFi.RSSI();
-//  Serial.print("signal strength (RSSI):");
-//  Serial.print(rssi);
-//  Serial.println(" dBm");
-//}
+void printWifiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
     
