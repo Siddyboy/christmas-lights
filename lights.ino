d5bb6348b3892644578d7fc692a41d5e93ab342a
simon <scjk@btinternet.com>
2020-03-01 11:17:44 +0000

    Sorting out file structure
    You have to put the Arduino sketch in a folder with the same name.

---
+++ lights.ino
@@ -0,0 +1,90 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+#include <DS3231.h>
+// #include <RTCZero.h>  // RTC.h or RTCZero.h?
+// Don't confuse connection to WiFi with connection to a server.
+#include "sub/arduino_secrets.h"
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int status = WL_IDLE_STATUS;
+
+unsigned int localPort = 2390;
+
+IPAddress timeServer(129, 6, 15, 28);
+
+const int NTP_PACKET_SIZE = 48;
+
+byte packetBuffer[NTP_PACKET_SIZE];
+
+// RTCZero rtc;
+WiFiUDP Udp;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+  Serial.println("Hello Simon");
+
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Comms with WiFi module failed!");
+    while (true);
+  }
+  
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+
+//  Serial.println("Connected to wifi");
+//  printWifiStatus();
+
+//  Serial.println("\nStarting connection to server...");
+//  Udp.begin(localUdpPort);
+  
+//  rtc.begin();
+
+  
+//  get time from ntp server
+//  set time to rtc
+}
+
+void loop() {
+  while(true);
+}
+//  timed on/off
+//  set hours plus randomness
+  
+//  at hours (when on (except 4 pm)) 
+//   turn off for few seconds
+//    blink hour 'chimes' - see big ben timing!
+    
+//  at quarters (when on)
+//   just blink off once (15 min past)
+//    blink off twice (30 past)
+//    blink off thrice (45 past).
+    
+//  at 4pm
+//    turn off for a few seconds
+//    blink tea-for-two rhythm.
+    
+//  at 3am get time from ntp and reset rtc
+  
+//void printWifiStatus() {
+//  Serial.print("SSID: ");
+// Serial.println(WiFi.SSID());
+
+//  IPAddress ip = WiFi.localIP();
+//  Serial.print("IP Address: ");
+//  Serial.println(ip);
+
+//  long rssi = WiFi.RSSI();
+//  Serial.print("signal strength (RSSI):");
+//  Serial.print(rssi);
+//  Serial.println(" dBm");
+//}
+    
